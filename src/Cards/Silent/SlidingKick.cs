8bc0526b08e6aa9da7dc3dc590d4f432a526a5a8
Arthur Lamidel <arthurlamidel@gmail.com>
2026-05-31 23:04:06 +0200

    Add `Sliding Kick`

---
+++ src/Cards/Silent/SlidingKick.cs
@@ -0,0 +1,45 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+
+namespace SecondDraw.Cards.Silent;
+
+[Pool(typeof(SilentCardPool))]
+public class SlidingKick() : CustomCard(0, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy) {
+	public override IEnumerable<CardKeyword> CanonicalKeywords => [];
+
+	protected override IEnumerable<DynamicVar> CanonicalVars => [
+		new DamageVar(4, ValueProp.Move),
+	];
+
+	protected override IEnumerable<IHoverTip> ExtraHoverTips => [];
+
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+		ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+
+		await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+			.FromCard(this)
+			.Targeting(cardPlay.Target)
+			.WithHitCount(DynamicVars.Repeat.IntValue)
+			.WithHitFx("vfx/vfx_attack_slash")
+			.Execute(choiceContext);
+
+		var prefs = new CardSelectorPrefs(SelectionScreenPrompt, 2);
+		var selected = await CardSelectCmd.FromHand(choiceContext, Owner, prefs, null, this);
+		foreach (var card in selected) {
+			await CardCmd.Discard(choiceContext, card);
+		}
+    }
+
+	protected override void OnUpgrade() {
+		DynamicVars.Damage.UpgradeValueBy(3);
+	}
+}
