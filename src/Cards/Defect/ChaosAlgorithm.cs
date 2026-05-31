d4d1017e2f2073c58a2d852b62ab0311876d83de
Arthur Lamidel <arthurlamidel@gmail.com>
2026-05-31 16:05:01 +0200

    Add first card

---
+++ src/Cards/Defect/ChaosAlgorithm.cs
@@ -0,0 +1,36 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+
+namespace SecondDraw.Cards.Defect;
+
+[Pool(typeof(DefectCardPool))]
+public abstract class ChaosAlgorithm() : CustomCard(0, CardType.Skill, CardRarity.Rare, TargetType.Self) {
+	protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+	protected override bool HasEnergyCostX => true;
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+		await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
+
+		int numOfOrbs = ResolveEnergyXValue();
+		if (base.IsUpgraded) {
+			numOfOrbs++;
+		}
+
+		if (true) {
+			numOfOrbs *= 2;
+		}
+		
+		for (int i = 0; i < numOfOrbs; i++) {
+			var randomOrb = OrbModel.GetRandomOrb(base.Owner.RunState.Rng.CombatOrbGeneration).ToMutable();
+			await OrbCmd.Channel(choiceContext, randomOrb, base.Owner);
+		}
+
+    }
+}
