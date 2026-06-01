6749a6af25be5a78eaf038ae976fc1543c1073dc
Arthur Lamidel <arthurlamidel@gmail.com>
2026-06-01 23:00:39 +0200

    Fixes unnecessary usings

--- src/Cards/Silent/PhantomStrike.cs
+++ src/Cards/Silent/PhantomStrike.cs
@@ -1,7 +1,5 @@
 
 using BaseLib.Utils;
-using MegaCrit.Sts2.Core.Combat;
-using MegaCrit.Sts2.Core.Combat.History.Entries;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
